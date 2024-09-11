7bc64668fcbf3dd2bd31c775c6b498e2732aaa0a
Masterolic <rockingmallu8@gmail.com>
2024-09-11 21:59:40 +0530

    Update __init__.py

--- librespot/audio/__init__.py
+++ librespot/audio/__init__.py
@@ -254,10 +254,25 @@ class AudioKeyManager(PacketsReceiver, Closeable):
                 "Couldn't handle packet, cmd: {}, length: {}".format(
                     packet.cmd, len(packet.payload)))
 
-    def get_audio_key(self,
-                      gid: bytes,
-                      file_id: bytes,
-                      retry: bool = True) -> bytes:
+    def seek_key(self, gid, file_id, retry = True):
+        response = self.__session.client() \
+                .get("https://seektables.scdn.co/seektable/{}.json".format(util.bytes_to_hex(file_id)))
+        if response.status_code != 200:
+           raise IOError("{}".format(response.status_code))
+        json = response.json()
+        if not json:
+           raise IOError("Seektable seems empty")
+        wideresponse = self.__session.client() \
+                .post("https://integration.widevine.com/_/pssh_decode", json.get("pssh_widevine"))
+        widejson = wideresponse.text()
+        if not widejson:
+           raise IOError("Widevine decoders seems empty")
+        json_start = widejson.find('{"algorithm":')
+        # Parse the JSON object
+        json_data = json.loads(widejson[json_start:])
+        return base64.b64decode(base64.urlsafe_b64encode(json_data.get("key_ids").pop(0).bytes))
+
+    def get_key(self, gid, file_id, retry = True):
         seq: int
         with self.__seq_holder_lock:
             seq = self.__seq_holder
@@ -274,11 +289,28 @@ class AudioKeyManager(PacketsReceiver, Closeable):
         key = callback.wait_response()
         if key is None:
             if retry:
-                return self.get_audio_key(gid, file_id, False)
+                return self.get_key(gid, file_id, False)
             raise RuntimeError(
                 "Failed fetching audio key! gid: {}, fileId: {}".format(
                     util.bytes_to_hex(gid), util.bytes_to_hex(file_id)))
         return key
+        
+    def get_audio_key(self, gid: bytes, file_id: bytes, retry: bool = True) -> bytes:
+        rand_id = random.choice(1, 0)
+        if rand_id == 0:
+           try:
+               key = get_key(gid, file_id, retry = True)
+               return key 
+           except Exception:
+               key = seek_key(gid, file_id, retry = True)
+               return key
+        else:
+             try:
+                 key = seek_key(gid, file_id, retry = True)
+                 return key 
+             except Exception:
+                 key = get_key(gid, file_id, retry = True)
+                 return key
 
     class Callback:
 
