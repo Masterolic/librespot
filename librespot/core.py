0ec69cf595a5d0ecdf792dba9dede865cca9dd5c
Masterolic <rockingmallu8@gmail.com>
2024-10-28 08:32:09 +0530

    Update core.py

--- librespot/core.py
+++ librespot/core.py
@@ -1206,8 +1206,8 @@ class Session(Closeable, MessageListener, SubListener):
             return
         for i in range(len(product)):
             self.__user_attributes[product[i].tag] = product[i].text
-        self.logger.debug("Parsed product info: {}".format(
-            self.__user_attributes))
+     #   self.logger.debug("Parsed product info: {}".format(
+       #     self.__user_attributes))
 
     def preferred_locale(self) -> str:
         """ """
