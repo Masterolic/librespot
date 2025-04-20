a0d3c82d9fd472cae6e19807cb41488b8b2b9557
Masterolic <rockingmallu8@gmail.com>
2025-04-20 21:59:54 +0530

    Update core.py

--- librespot/core.py
+++ librespot/core.py
@@ -1893,12 +1893,12 @@ class Session(Closeable, MessageListener, SubListener):
             sock = socket.socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
             sock.settimeout(9)
             try:
-                sock.connect((ap_address, ap_port))
+                sock.connect((ap_address, 443))
             except Exception:
                 address = ApResolver.get_random_accesspoint()
                 ap_address = address.split(":")[0]
                 ap_port = int(address.split(":")[1])
-                sock.connect((ap_address, ap_port))
+                sock.connect((ap_address, 443))
             return Session.ConnectionHolder(sock)
     
         def close(self) -> None:
